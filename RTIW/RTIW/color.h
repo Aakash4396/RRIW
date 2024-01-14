d3a72a30dff0411104513c1e8ed3fbf068597b8c
Aakash <aakashpawar4396>
2024-01-14 18:56:41 +0530

    modified color.h

--- RTIW/RTIW/color.h
+++ RTIW/RTIW/color.h
@@ -13,7 +13,6 @@ void write_color(unsigned char* image, int col, int row, int width, color pxl) {
     image[(col * width + row) * 4 + 1] = static_cast<unsigned char>(255.999 * pxl.y());
     image[(col * width + row) * 4 + 2] = static_cast<unsigned char>(255.999 * pxl.z());
     image[(col * width + row) * 4 + 3] = 255;
-    //fout << (int)image[(col * width + row) * 4 + 0] << " " << (int)image[(col * width + row) * 4 + 1] << " " << (int)image[(col * width + row) * 4 + 2] << fflush;
 }
 
 #endif // !COLOR_H
