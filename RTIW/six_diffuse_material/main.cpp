ac0bfc9e3ab9c337bafa8e3d65903711be6b35cc
Aakash <aakashpawar4396>
2024-01-17 22:09:58 +0530

    Diffuse material folder

---
+++ RTIW/six_diffuse_material/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "Renderer.h"
+
+#include "FileWriterUtility.h"
+#include "rtiw.h"
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+
+int main() {
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point(0.0f, 0.0f, -1.0f), 0.5f));
+    world.add(make_shared<sphere>(point(0.0f, -100.5f, -1.0f), 100.0f));
+
+    auto aspect_ratio = 16.0 / 9.0;
+    int width = 800;
+    int height = static_cast<int>(width / aspect_ratio);
+
+    camera cam(width, height);
+
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    unsigned char* imageData = cam.createImage(world);
+
+    Renderer rdr;
+    bool retVal = rdr.createWindow(L"RTIW", width, height);
+
+    if (retVal) {
+        rdr.drawImage(imageData);
+        rdr.RunMessageLoop();
+    }
+
+    fpclose;
+    return 0;
+}
+
+
