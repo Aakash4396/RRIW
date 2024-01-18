fdfd7cabfcb54a3e4dd3d2e4adff0f1ef5bd4be4
Aakash <aakashpawar4396>
2024-01-18 09:55:16 +0530

    Defocus blurr folder

---
+++ RTIW/nine_defocus_blurr/FileWriterUtility.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+std::ofstream fout("img9.ppm");
+
+#define fp(a) fout << #a " = " << a << fflush
+#define fpclose fout.close()
+
+typedef void(*fendlManipulatorFunc)(std::ofstream&, int);
+typedef std::ofstream& (*fflushManipulatorFunc)(std::ofstream&, int, int);
+
+template <class _CharT, class _Traits>
+std::basic_ostream<_CharT, _Traits>& operator<<(std::basic_ostream<_CharT, _Traits>& fout, fendlManipulatorFunc m) {
+    m(reinterpret_cast<std::ofstream&>(fout), 0);
+    return fout;
+}
+
+template <class _CharT, class _Traits>
+std::basic_ostream<_CharT, _Traits>& operator<<(std::basic_ostream<_CharT, _Traits>& fout, fflushManipulatorFunc m) {
+    m(reinterpret_cast<std::ofstream&>(fout), 0, 0);
+    return fout;
+}
+
+void fendl(std::ofstream& fout, int) {
+    fout.put('\n').flush();
+    fout.close();
+}
+
+std::ofstream& fflush(std::ofstream& fout, int, int) {
+    fout.put('\n').flush();
+    return fout;
+}
+
+
---
+++ RTIW/nine_defocus_blurr/rtiw.h
@@ -0,0 +1,89 @@
+#pragma once
+#ifndef RTIW_H
+#define RTIW_H
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+// Common Headers
+
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+#include "color.h"
+#include "hittable.h"
+
+#endif // !RTIW_H
+
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1.0, 1.0);
+        if (p.length_squared() < 1) {
+            return p;
+        }
+    }
+}
+
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+
+vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
+
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
