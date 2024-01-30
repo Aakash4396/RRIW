83eb314d1e03fbd4d6f8f73261a56c56c6655827
Aakash <aakashpawar4396>
2024-01-31 01:30:47 +0530

    Defocus blurr

---
+++ RTIW_HPP/09_Defocus_blurr/hittable_list.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+#include "PStash.h"
+#include "interval.h"
+#include "sphere.h"
+
+class hittable_list {
+public:
+    PStash<hittable *> objects;
+    
+    __device__ hittable_list() {}
+    __device__ hittable_list(hittable* object) { add(object); }
+
+    __device__ void add(hittable* object) {
+        objects.add(object);
+    }
+
+    __device__ bool hit(const Ray& r, interval ray_t, hit_record& rec) {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_t.imax;
+
+        for (auto object : objects) {
+            if (object->hit(r, interval(ray_t.imin, closest_so_far), temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+
+};
+
+
+#endif // !HITTABLE_LIST_H
